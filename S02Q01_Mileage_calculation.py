6380b7a132f9773178c11ebd56e076c3baea93a8
devinenv <80189537+devinenv@users.noreply.github.com>
2021-03-09 00:32:53 +0530

    Add files via upload
    written code to calculate mileage with pre-defined inputs(static values)

---
+++ S02Q01_Mileage_calculation.py
@@ -0,0 +1,6 @@
+Start_value=100
+End_value=1000
+fuel_consumed=20
+Total_Distance_Travelled=End_value-Start_value
+Mileage=Total_Distance_Travelled/fuel_consumed
+print("Mileage per liter:", int(Mileage))
